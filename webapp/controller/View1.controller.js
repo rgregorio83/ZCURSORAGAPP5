4bfc2b889cc84dcfd1eb19c802d149ff70a238fc
Rodrigo <rodrigo.ariza.gregorio@gmail.com>
2024-04-16 08:04:06 -0300

    Botao Update

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -12,6 +12,56 @@ sap.ui.define([
 
             },
 
+
+            onUpdate: function () {
+                var that = this;
+                var usuario = this.getView().byId("idusuario").getValue();
+                var nome = this.getView().byId("idNome").getValue();
+                var projesegw = this.getView().byId("idProjSegw").getValue();
+                var email = this.getView().byId("idEmail").getValue();
+ 
+                var oStrPath = this.getView().byId("idTableAluno").getSelectedContextPaths();
+
+                var oDados = {
+                    Usuario: usuario,
+                    Nome: nome,
+                    ProjetoSegw: projesegw,
+                    Email: email
+                }
+ 
+                this.getView().getModel().update(oStrPath[0], oDados, {
+                    success: function (oData, oReponse) {
+                        sap.m.MessageBox.success(that.getView().getModel("i18n").getResourceBundle().getText("lblmsgUpdateOk"));
+                        that.getView().byId("idusuario").setValue("");
+                        that.getView().byId("idNome").setValue("");
+                        that.getView().byId("idProjSegw").setValue("");
+                        that.getView().byId("idEmail").setValue("");
+                    },
+                    error: function (oError) {
+                        sap.m.MessageBox.error(that.getView().getModel("i18n").getResourceBundle().getText("lblmsgUpdateError"));
+                    }
+                });
+            },
+
+
+
+
+            onDelete: function() {
+                var that = this;
+                var sPath = this.getView().byId("idTableAluno").getSelectedContextPaths();
+
+                this.getView().getModel().remove(sPath[0], { 
+                    success: function(odata, oResponse){
+                    sap.m.MessageBox.success(that.getView().getModel("i18n").getResourceBundle().getText("lblmsgRemoveOk"));
+                    return;
+                },
+                error: function(oError){
+
+                }
+
+                })
+            },
+
             onCreate: function() {
                 var that = this;
                 var usuario  = this.getView().byId("idusuario").getValue();
